51eb3490151deed1ad47725b40dd4187b9519be5
mgirouard <mgirouard@6f5f4c61-da3f-0410-8e3f-13ff87d4f015>
2008-04-29 05:28:27 +0000

    Moved.

--- incubator/Request/Interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface Panda_Request_Interface
-{
-	public static function singleton($Loader = null);
-	public function getMethod();
-	public function getURI();
-	public function getData();
-}
