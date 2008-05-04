53b8ba701983a8cbe9f2824c3348d23f81d1bc16
mgirouard <mgirouard@6f5f4c61-da3f-0410-8e3f-13ff87d4f015>
2008-05-04 00:31:53 +0000

    Created a singleton version of the loader which affords a simple autoloading mechanism.

---
+++ Panda/Loader/Singleton.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'Panda/Loader/Abstract.php';
+
+/**
+ * A singleton source loader
+ *
+ * @package Panda_Loader
+ * @author  Michael Girouard
+ * @license The New BSD License (http://pandaphp.org/license.html)
+ */
+class Panda_Loader_Singleton
+extends Panda_Loader_Abstract
+{
+    /**
+     * Returns a singleton instance
+     *
+     * @return Panda_Loader_Singleton
+     */
+	public static function singleton()
+	{
+		static $Instance;
+		
+		if ($Instance === null) {
+			$Instance = new self;
+		}
+		
+		return $Instance;
+	}
+	
+	/**
+	 * An autoloader which for use with SPL
+	 *
+	 * @param string $className
+	 * @return bool
+	 */
+	public static function autoload($className)
+	{
+	    return self::singleton()->load($className);
+	}
+}
