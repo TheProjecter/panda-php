79e303909b0c9db47af0cb926861cadd04b88cfa
mgirouard <mgirouard@6f5f4c61-da3f-0410-8e3f-13ff87d4f015>
2008-08-03 22:44:49 +0000

    Deleted incubator files in favor of the incubator branch.

--- incubator/Panda/Factory/Interface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface Panda_Factory_Interface
-{
-    public static function factory(array $configuration = array());
-}
--- incubator/Panda/Singleton/Interface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface Panda_Singleton_Interface
-{
-    public static function singleton(array $configuration = array());
-}
