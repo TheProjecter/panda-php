69c5b2666e6863a855683f8bbd2af31e68ca80af
mgirouard <mgirouard@6f5f4c61-da3f-0410-8e3f-13ff87d4f015>
2008-08-30 20:58:23 +0000

    Documented

--- trunk/Panda/AjaxService.php
+++ trunk/Panda/AjaxService.php
@@ -1,13 +1,55 @@
 <?php
 
+/**
+ * Creates a new Ajax Service from PHP Classes
+ *
+ * @package Panda
+ * @author Michael Girouard (mikeg@lovemikeg.com)
+ * @license The New BSD License (http://pandaphp.org/license.html)
+ */
 class Panda_AjaxService
 {
+    /**
+     * The class name to use as the service
+     *
+     * @var string
+     */
 	private $name;
+
+	/**
+	 * The name of the JavaScript object
+	 *
+	 * @var string
+	 */
 	private $jsName;
+
+	/**
+	 * A list of all public properties for the service
+	 *
+	 * @var array
+	 */
 	private $properties = array();
+
+	/**
+	 * A list of all public methods for the service
+	 *
+	 * @var array
+	 */
 	private $methods = array();
+
+	/**
+	 * The template JavaScript document used by the service
+	 *
+	 * @var string
+	 */
 	private $serviceTemplate;
 
+	/**
+	 * Constructs a new Ajax Service
+	 *
+	 * @param string $name
+	 * @param string $jsName
+	 */
 	public function __construct($name, $jsName = false)
 	{
 		$this->name = $name;
@@ -22,6 +64,11 @@ class Panda_AjaxService
 		$this->captureClassMethods($name);
 	}
 
+	/**
+	 * Object to string converter
+	 *
+	 * @return string
+	 */
 	public function __toString()
 	{
 		$serviceDefinition = '';
@@ -42,21 +89,43 @@ class Panda_AjaxService
 		return $template;
 	}
 
+	/**
+	 * Sets a property in the service
+	 *
+	 * @param string $name
+	 * @param s $value
+	 */
 	public function setProperty($name, $value)
 	{
 		$this->properties[$name] = $value;
 	}
 
+	/**
+	 * Unsets a property in the service
+	 *
+	 * @param unknown_type $name
+	 */
 	public function unsetProperty($name)
 	{
 		unset($this->properties[$name]);
 	}
 
+	/**
+	 * Adds a method to the service
+	 *
+	 * @param string $name
+	 * @param array $params
+	 */
 	public function addMethod($name, array $params = array())
 	{
 		$this->methods[$name] = $params;
 	}
 
+	/**
+	 * Removes a method from the service
+	 *
+	 * @param string $name
+	 */
 	public function removeMethod($name)
 	{
 		if ($key = array_search($name, $this->methods)) {
@@ -64,6 +133,12 @@ class Panda_AjaxService
 		}
 	}
 
+	/**
+	 * Captures all class methods via reflection
+	 *
+	 * @param string $className
+	 * @param boolean $autoload
+	 */
 	public function captureClassMethods($className, $autoload = true)
 	{
 		if (class_exists($className, $autoload)) {
@@ -86,6 +161,11 @@ class Panda_AjaxService
 		}
 	}
 
+	/**
+	 * Returns the current service template, lazy-loading if necessary
+	 *
+	 * @return string
+	 */
 	private function getServiceTemplate()
 	{
 		if (empty($this->serviceTemplate)) {
@@ -103,6 +183,11 @@ class Panda_AjaxService
 		return $this->serviceTemplate;
 	}
 
+	/**
+	 * Renders all properties in the service
+	 *
+	 * @return string
+	 */
 	private function renderAllProperties()
 	{
 		$out = array();
@@ -114,6 +199,11 @@ class Panda_AjaxService
 		return implode(",\n", $out);
 	}
 
+	/**
+	 * Renders all methods in the service
+	 *
+	 * @return string
+	 */
 	private function renderAllMethods()
 	{
 		$out = array();
@@ -125,6 +215,13 @@ class Panda_AjaxService
 		return implode(",\n", $out);
 	}
 
+	/**
+	 * Renders a service method
+	 *
+	 * @param string $name
+	 * @param array $params
+	 * @return string
+	 */
 	private function renderMethod($name, $params)
 	{
 		return sprintf(
@@ -134,6 +231,13 @@ class Panda_AjaxService
 		);
 	}
 
+	/**
+	 * Renders the body of a method
+	 *
+	 * @param string $name
+	 * @return string
+	 * @todo This should be abstracted into a separate class
+	 */
 	private function renderMethodBody($name)
 	{
 		return "sendRequest('$name', arguments);";
