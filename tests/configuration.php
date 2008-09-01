5a88a90bbd4f0b20397ba9f0a11ef54ab3514858
mgirouard <mgirouard@6f5f4c61-da3f-0410-8e3f-13ff87d4f015>
2008-09-01 14:11:45 +0000

    Created test configuration script.

---
+++ tests/configuration.php
@@ -0,0 +1,6 @@
+<?php
+
+$baseDir = '/Users/mgirouard/Sites/panda-php/branches/incubator/trunk';
+//$baseDir = '/Users/mgirouard/Sites/panda-php/trunk';
+
+set_include_path($baseDir . PATH_SEPARATOR . get_include_path());
